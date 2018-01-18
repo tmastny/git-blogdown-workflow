5e73d50a1bbf7b74e61e77d37054a6a435907420
Timothy Mastny <tim.mastny@gmail.com>
2018-01-18 17:59:53 -0600

    added chunk examples

---
+++ chunks.R
@@ -0,0 +1,36 @@
+## ---- library_calls ----
+library(tidyverse)
+library(magrittr)
+
+## ---- simulate_function ----
+monty_hall_sim <- function (door, switch) {
+  doors <- c('a', 'b', 'c')
+  prize <- sample(doors, size = 1)
+  open_door <- sample(setdiff(doors, c(prize, door)), size = 1)
+  if (switch) {
+    door <- setdiff(doors, c(door, open_door))
+  }
+  return(door == prize)
+}
+
+## ---- run_simulation ----
+d <- tibble(
+  door = sample(c('a', 'b', 'c'), size = 1000, replace = TRUE),
+  switch = sample(c(TRUE, FALSE), size = 1000, replace = TRUE)
+)
+d %<>%
+  mutate(prize = mapply(monty_hall_sim, door, switch))
+
+## ---- plot_results ----
+d %>%
+  group_by(switch) %>%
+  mutate(success_ratio = cumsum(prize)/n()) %>%
+  ungroup() %>%
+  rowid_to_column(var = 'attempt') %>%
+  ggplot() +
+  geom_line(aes(x = attempt, y = success_ratio, color = switch))
+
+
+
+
+
---
+++ fit_model.R
@@ -0,0 +1,16 @@
+## ---- library_call ----
+library(tidyverse)
+library(magrittr)
+
+## ---- read_data ----
+d <- read_csv(file = 'tidied_periodization.csv')
+
+## ---- data_cleaning ----
+
+
+## ---- fit_model ----
+
+library(brms)
+
+
+## ---- save_fit ----
