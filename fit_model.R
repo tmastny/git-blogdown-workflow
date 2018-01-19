baa5c33c7432d365d7a201e31c12e80111ae96f6
Timothy Mastny <tim.mastny@gmail.com>
2018-01-19 11:30:43 -0600

    built model and saved RDS

--- fit_model.R
+++ fit_model.R
@@ -1,16 +1,24 @@
 ## ---- library_call ----
 library(tidyverse)
-library(magrittr)
 
-## ---- read_data ----
+## ---- read_data_error ----
 d <- read_csv(file = 'tidied_periodization.csv')
 
-## ---- data_cleaning ----
-
+## ---- read_data_good ----
+d <- read_csv(file = here::here('tidied_periodization.csv'))
 
 ## ---- fit_model ----
-
-library(brms)
-
+m <- lm(post ~ pre, data = d)
 
 ## ---- save_fit ----
+saveRDS(m, file = 'saved_model')
+
+## ---- plot_model ----
+d %>%
+  mutate(pred = predict(m, newdata = .)) %>%
+  ggplot(aes(x = pre)) +
+  geom_point(aes(y = post)) +
+  geom_line(aes(y = pred)) + 
+  xlim(0, 420) + ylim(0, 500)
+  
+  
